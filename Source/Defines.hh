8a96e1a7d7eee0b4a36b67ebc2352ddb70752254
Floyden <floyden@fnaa.net>
2020-10-20 08:21:25 +0200

    Add Queue and Deque

--- Source/Defines.hh
+++ Source/Defines.hh
@@ -3,26 +3,34 @@
 #include <vector>
 #include <string>
 #include <array>
+#include <deque>
+#include <queue>
 
 #include <unordered_set>
 #include <unordered_map>
 
-template<typename T, size_t N>
+template<class T, size_t N>
 using Array = std::array<T, N>;
 
-template<typename T>
+template<class T>
 using Vector = std::vector<T>;
 
-template<typename T>
+template<class T, class A = std::allocator<T>>
+using Deque = std::deque<T, A>;
+
+template<class T, class C = Deque<T>>
+using Queue = std::queue<T, C>;
+
+template<class T>
 using SPtr = std::shared_ptr<T>;
 
-template<typename T>
+template<class T>
 using UPtr = std::unique_ptr<T>;
 
-template<typename T, typename H = std::hash<T>, typename E = std::equal_to<T>>
+template<class T, class H = std::hash<T>, class E = std::equal_to<T>>
 using UnorderedSet = std::unordered_set<T, H, E>;
 
-template<typename K, typename V, typename H = std::hash<K>, typename E = std::equal_to<K>>
+template<class K, class V, class H = std::hash<K>, class E = std::equal_to<K>>
 using UnorderedMap = std::unordered_map<K, V, H, E>;
 
 using String = std::string;
